8aa037348e55e5b300ebe58f0453c3914b812845
gabrieldemarmiesse <gabreil.de-marmiesse@valeo.com>
2019-09-27 15:51:01 +0200

    Reorganized tests.

--- tests/test_keras_integration.py
+++ tests/test_keras_integration.py
@@ -3,10 +3,13 @@ from docs.structure import PAGES
 import os
 
 
+def make_keras_docs(dest_dir):
+    generate(str(dest_dir), PAGES)
+
+
 def test_docs_in_custom_destination_dir(tmpdir):
-    generate(str(tmpdir), PAGES)
+    make_keras_docs(tmpdir)
     assert os.path.isdir(os.path.join(tmpdir, 'layers'))
     assert os.path.isdir(os.path.join(tmpdir, 'models'))
     assert os.path.isdir(os.path.join(tmpdir, 'examples'))
     assert os.listdir(os.path.join(tmpdir, 'examples'))
-
