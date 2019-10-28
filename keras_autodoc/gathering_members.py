0fc8fa1e6f6e0fa6f95e67a310b99fa6d5e3c12d
Gabriel de Marmiesse <gabrieldemarmiesse@gmail.com>
2019-10-28 16:19:21 +0100

    Some docstrings for public functions. (#34)

--- keras_autodoc/gathering_members.py
+++ keras_autodoc/gathering_members.py
@@ -1,17 +1,74 @@
 import inspect
 from inspect import isclass, isfunction, isroutine
+from typing import List
 from .utils import import_object
 
 
-def get_classes(module, exclude=None, return_strings=True):
+def get_classes(module,
+                exclude: List[str] = None,
+                return_strings: bool = True):
+    """Get all the classes of a module.
+
+    # Arguments
+
+    module (str or module): The module to fetch the classes from. If it's a
+        string, it should be in the dotted format. `'keras.layers'` for example.
+    exclude: The names which will be excluded from the returned list. For
+        example, `get_classes('keras.layers', exclude=['Dense', 'Conv2D']`.
+    return_strings: If False, the actual classes will be returned. Note that
+        if you use aliases when building your docs, you should use strings.
+        This is because the computed signature uses
+        `__name__` and `__module__` if you don't provide a string as input.
+
+    # Returns
+
+     A list of strings or a list of classes.
+    """
     return _get_all_module_element(module, exclude, return_strings, True)
 
 
-def get_functions(module, exclude=None, return_strings=True):
+def get_functions(module,
+                  exclude: List[str] = None,
+                  return_strings: bool = True):
+    """Get all the functions of a module.
+
+    # Arguments
+
+    module (str or module): The module to fetch the functions from. If it's a
+        string, it should be in the dotted format. `'keras.backend'` for example.
+    exclude: The names which will be excluded from the returned list. For
+        example, `get_functions('keras.backend', exclude=['max']`.
+    return_strings: If False, the actual functions will be returned. Note that
+        if you use aliases when building your docs, you should use strings.
+        This is because the computed signature uses
+        `__name__` and `__module__` if you don't provide a string as input.
+
+    # Returns
+
+     A list of strings or a list of functions.
+    """
     return _get_all_module_element(module, exclude, return_strings, False)
 
 
 def get_methods(cls, exclude=None, return_strings=True):
+    """Get all the method of a class.
+
+    # Arguments
+
+    cls (str or class): The class to fetch the methods from. If it's a
+        string, it should be in the dotted format. `'keras.layers.Dense'`
+        for example.
+    exclude: The names which will be excluded from the returned list. For
+        example, `get_methods('keras.Model', exclude=['save']`.
+    return_strings: If False, the actual methods will be returned. Note that
+        if you use aliases when building your docs, you should use strings.
+        This is because the computed signature uses
+        `__name__` and `__module__` if you don't provide a string as input.
+
+    # Returns
+
+     A list of strings or a list of methods.
+    """
     if isinstance(cls, str):
         cls_str = cls
         cls = import_object(cls)
