c282a4fe8d58386fcb5b3985a27e31a586c45433
Gabriel de Marmiesse <gabrieldemarmiesse@gmail.com>
2019-11-10 22:55:05 +0100

    Improving the setup.py for a better visibility on pypi. (#54)

--- setup.py
+++ setup.py
@@ -1,14 +1,33 @@
 from setuptools import setup
 from setuptools import find_packages
+from pathlib import Path
+
+this_file = Path(__file__).resolve()
+readme = this_file.parent / 'README.md'
 
 
 setup(
-    name="keras-autodoc",
-    version="0.3.2",
-    description="Building the Keras projects docs.",
-    author="The Keras team",
-    license="MIT",
-    install_requires=["markdown", 'sphinx', 'black==19.10b0'],
-    extras_require={"tests": ["pytest", "pytest-pep8"]},
+    name='keras-autodoc',
+    version='0.3.2',
     packages=find_packages(),
+    install_requires=['markdown', 'sphinx', 'black==19.10b0'],
+    package_data={'': ['README.md']},
+    author='The Keras team',
+    author_email='gabrieldemarmiesse@gmail.com',
+    description='Building the Keras projects docs.',
+    long_description=readme.read_text(encoding='utf-8'),
+    url='https://github.com/keras-team/keras-autodoc',
+    license='Apache License 2.0',
+    extras_require={'tests': ['pytest', 'pytest-pep8']},
+    classifiers=[
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Utilities',
+        'Topic :: Documentation',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: Apache Software License'
+    ]
 )
