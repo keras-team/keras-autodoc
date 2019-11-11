e94388e618e8dcc97f610410e6c1108f4f86a096
Gabriel de Marmiesse <gabrieldemarmiesse@gmail.com>
2019-11-11 15:57:08 +0100

    Added a test failing with the docstring conversion. (#55)

---
+++ tests/test_docstring.py
@@ -0,0 +1,101 @@
+from keras_autodoc.docstring import process_docstring
+import pytest
+
+
+docstring1 = """This is a docstring
+
+Some text is here.
+
+# Arguments
+
+    arg1: some argument.
+    arg2: Some other
+        argument with a line break.
+
+Some more text.
+"""
+
+expected1 = """This is a docstring
+
+Some text is here.
+
+__Arguments__
+
+
+- __arg1__: some argument.
+- __arg2__: Some other
+    argument with a line break.
+
+Some more text.
+"""
+
+
+def test_generate_docstring_basic():
+    computed = process_docstring(docstring1)
+    assert computed == expected1
+
+
+docstring2 = """This is a docstring
+
+Some text is here.
+
+# Arguments
+
+    arg1: some argument
+        here written: with colon
+
+Some more text.
+"""
+
+expected2 = """This is a docstring
+
+Some text is here.
+
+__Arguments__
+
+
+- __arg1__: some argument
+    here written: with colon
+
+Some more text.
+"""
+
+
+def test_generate_docstring_with_colon():
+    computed = process_docstring(docstring2)
+    assert computed == expected2
+
+
+docstring3 = """This is a docstring
+
+Some text is here.
+
+# Arguments
+
+    arg1: some argument.
+
+    arg2: Some other
+        argument with a line break.
+
+Some more text.
+"""
+
+expected3 = """This is a docstring
+
+Some text is here.
+
+__Arguments__
+
+
+- __arg1__: some argument.
+- __arg2__: Some other
+    argument with a line break.
+
+Some more text.
+"""
+
+
+@pytest.mark.xfail
+def test_generate_docstring_with_empty_line():
+    computed = process_docstring(docstring3)
+    assert computed == expected3
